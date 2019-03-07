f9126d251f7863636e88de19fea818177e83512f
Juliano sales <jsserra21@gmail.com>
2019-03-07 07:46:15 -0300

    DepartmentForm (dialog) design

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,18 +1,27 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.util.Alerts;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Department;
 import model.services.DepartmentService;
@@ -36,8 +45,9 @@ public class DepartmentListController implements Initializable{
 	private ObservableList<Department> obsList; //Vou carregar meus departamentos nessa ObservableList
 	
 	@FXML
-	public void onBtNewAction() {
-		System.out.println("onBtNewAcion");
+	public void onBtNewAction(ActionEvent event) {
+		Stage parentStage = Utils.currentStage(event);
+		createDialogForm("/gui/DepartmentForm.fxml", parentStage);
 	}
 	
 	public void setDepartmentService(DepartmentService service) {
@@ -70,6 +80,33 @@ public class DepartmentListController implements Initializable{
 		//Macete para TableView acompanhar altura da janela, Instancio stage recebendo janela da cena da minha classe Main
 		Stage stage = (Stage) Main.getMainScene().getWindow();
 		tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
+	}
+	
+	/* Nossa função pra carregar a janela do formulário para preencher novo departamento
+	 *Esse método vai receber como parâmetro Stage pq quando agente cria um tela de diálogo
+	 agente tem que informar que é o Stage (pai) que criou essa janela de diálogo*/
+	public void createDialogForm(String absoluteName, Stage parentStage) {
+		try {
+			//vamos criar aqui nossa lógica para abrir a tela de formulário
+			// pra carregar uma tela precisamos do objeto da classe FXMLLoader
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));/* O getCalss é padrão da classe
+			FXMLLoader. Qual recurso que vamos pegar? É o que vem como parâmetro */
+			Pane pane = loader.load();
+			//Agora, que vai entrar o stage, quando vou carregar uma janela de diálogo modal
+			//na frente de uma janela existente eu tenho que instanciar um novo stage, logo vai ser 
+			//um palco na frente do outro
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Enter Department Data");
+			dialogStage.setScene(new Scene(pane)); //Quem vai ser o elemento raiz dessa cena? R: Pane
+			dialogStage.setResizable(false); //Essa função é para dizer se a janela pode ou não ser redimensionada
+			dialogStage.initOwner(parentStage); //Quem que é o Stage pai dessa janela? R: parentStage
+			dialogStage.initModality(Modality.WINDOW_MODAL);//Esse método que vai dizer se a janela vai ser modal ou se vai ter outro comportamento
+			//nesse caso vou travar ele para não poder mexer na janela anterior até que essa seja fechada
+			dialogStage.showAndWait();
+		}catch(IOException e)  {
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
+		
 	}
 
 }
---
+++ src/gui/util/Utils.java
@@ -0,0 +1,22 @@
+package gui.util;
+
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
+public class Utils {
+	
+	/* Criando médodo currentStage "palco atual" para retornar um stage (palco),
+	 * esse método vai receber como argumento ActionEvent que é evento que o botão recebeu,
+	 * Para eu acessar o stage onde meu controler que recebeu o evento está. Ex.: Se eu clico no botão
+	 * eu vou pegar o stage daquele botão
+	 */
+	public static Stage currentStage(ActionEvent event) {
+		//1 retornar event.getSource() porém o getSource é um objeto muito genérico
+		//2 Então vou transformalo em Node fazendo um Downcast e coloco tudo entre parênteses
+		//3 A partir desse node vou chamar a função getScene() para pegar a cena
+		//4 depois chamo a função getWindow() para chamar a tela fazendo um downcast para Stage
+		return (Stage)((Node)event.getSource()).getScene().getWindow();
+	}
+
+}
